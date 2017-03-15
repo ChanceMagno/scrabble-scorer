741de64ca8c5ef3911050e31c3fe9f62ac4ed1a2
Chance Magno and Nathaniel Meyer <nathaniel@>
2017-03-15 10:38:43 -0700

    Implement command line interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,17 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
+    Console myConsole = System.console();
+    System.out.println("I'm a scrabble scorer! type a single word in and I will output the score");
 
-    System.out.println("I'm a scrabble scorer!");
+    String userInputtedWord = myConsole.readLine();
+
+
+    ScrabbleScorer wordScore = new ScrabbleScorer();
+    Integer score = wordScore.calculateWordScore(userInputtedWord);
+
+    System.out.println(score);
 
   }
 }
